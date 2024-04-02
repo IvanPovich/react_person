2f06dfb97523d92f06e7d98eb7566b9960e1962f
Ivan <110199ws@gmail.com>
2024-04-02 10:46:41 +0300

    task Person solution, const PErson fixed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,17 +1,17 @@
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
 
+  const agePerson = age ? <p className="Person__age">I am {age}</p> : null;
+  const personPartnerType = sex === 'm' ? 'wife' : 'husband';
+  const personPartner = isMarried
+    ? `${partnerName} is my ${personPartnerType}`
+    : 'I am not married';
+
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
-      {age && <p className="Person__age">{`I am ${age}`}</p>}
-      {isMarried ? (
-        <p className="Person__partner">
-          {partnerName} is my {sex === 'm' ? 'wife' : 'husband'}
-        </p>
-      ) : (
-        <p className="Person__partner">I am not married</p>
-      )}
+      {agePerson}
+      <p className="Person__partner">{personPartner}</p>
     </section>
   );
 };
